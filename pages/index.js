610f4b9cfa48b49b0fd02fb04f132f7d2b1e72f0
Matheus Arnaud Pina Baker <matheus_baker@hotmail.com>
2021-07-01 00:51:49 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import axios from 'axios';
-
+import Head from 'next/head';
+import cards from '../styles/cards.module.css';
 const Index = ({dados}) => (
   <div>
+    <Head>
+    <title>Consulte aqui</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1>Lista de Nomes</h1>
     {console.log(dados)};
   <ul>
+    <div className={cards.row}>
+      <div className={cards.column}>
     {dados.DadosListTitulos.map(nomes=>(
-      <li key={nomes.dtr_id}>
-        <h2>{nomes.dtr_nomedevedor}</h2>
-      </li>
-    ))}
+      <div key={nomes.dtr_id}>
+        <h2 className = {cards.cardtxt}>{nomes.dtr_nomedevedor}
+        </h2>
+      </div>
+      ))}
+      </div>
+    </div>
   </ul>
   
   </div>
